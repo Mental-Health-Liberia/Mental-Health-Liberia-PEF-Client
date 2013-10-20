2d8dde8ecd766e0e2fd87cbce74ad70eb5c29940
Tanner Smith <tanner@tssoftware.net>
2013-10-20 09:30:03 -0400

    Get successfull node/npm/bundle/grunt/bower Puppet manifest working.
    Manifest now installs all required dependencies and builds the site.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,24 +1,73 @@
 class do-stuff {
   include apt
 
-  exec { 'first update':
+  exec { 'update':
     command => '/usr/bin/apt-get update'
   }
-  
-  apt::ppa { 'ppa:chris-lea/node.js':
-    require => Exec['first update'],
-  }
 
-  exec { 'second update':
-    command => '/usr/bin/apt-get update',
-    require => Apt::Ppa['ppa:chris-lea/node.js'],
-  }
-
-  package {  [ 'build-essential', 'git', 'nodejs' ]:
+  package { [ 'build-essential', 'git', 'python', 'g++', 'make', 'checkinstall' ]:
     ensure  => present,
-    require => Exec['second update'],
+    require => Exec['update'],
   }
 
+  exec { 'get node':
+    command => '/usr/bin/wget -N http://nodejs.org/dist/node-latest.tar.gz',
+    cwd => '/tmp',
+  }
+
+  file { '/tmp/node-latest':
+    ensure => 'directory'
+  }
+
+  exec { 'decompress node':
+    command => '/bin/tar xzvf node-latest.tar.gz --strip-components 1 -C /tmp/node-latest',
+    cwd => '/tmp',
+    require => [ Exec['get node'], File['/tmp/node-latest'] ]
+  }
+
+  exec { 'configure node':
+    command => '/usr/bin/python configure',
+    cwd => '/tmp/node-latest',
+    require => [ Exec['decompress node'], Package['python'] ],
+  }
+
+  exec { 'make':
+    command => '/usr/bin/make && /usr/bin/make install',
+    cwd => '/tmp/node-latest',
+    require => [ Exec['configure node'], Package['make'], Package['build-essential'], Package['g++'] ],
+  }
+
+  exec { 'install grunt and bower':
+    command => '/usr/local/bin/npm install -g grunt-cli bower',
+    require => Exec['make'],
+  }
+
+  exec { 'install nodemon':
+    command => '/usr/local/bin/npm install -g nodemon',
+    require => Exec['make']
+  }
+
+  exec { 'install bundle':
+    command => '/opt/vagrant_ruby/bin/gem install bundler',
+  }
+
+  exec { 'install dependencies':
+    command => '/usr/local/bin/npm install; /usr/local/bin/bower --allow-root install',
+    cwd => '/vagrant',
+    require => [ Exec['make'], Exec['install grunt and bower'] ],
+  }
+
+  exec { 'install required gems':
+    command => '/opt/vagrant_ruby/bin/bundle',
+    cwd => '/vagrant',
+    require => Exec['install bundle'],
+  }
+
+  exec { 'build site':
+    command => '/usr/local/bin/grunt build',
+    cwd => '/vagrant',
+    require => Exec['install dependencies'],
+  }
 }
 
 include do-stuff
